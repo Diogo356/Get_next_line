6be6f718fb0bceb44cb1a1afa6fa33565c60897a
Diogo <diogobelar262@gmail.com>
2022-10-07 20:07:26 +0200

    inicio do get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbelarmi <dbelarmi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 17:47:44 by dbelarmi          #+#    #+#             */
+/*   Updated: 2022/10/07 19:10:28 by dbelarmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+//função para ler do descritor de arquivos;
+//está função lerá o descritor de arquivos em um loop até detectarmos o \n ou
+//o final do arquivo:
+
+int		write_fd(char *fd)
+{
+	while
+}
+
+char	*get_next_line(int fd)
+{
+	write_fd(fd);
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbelarmi <dbelarmi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 17:48:08 by dbelarmi          #+#    #+#             */
+/*   Updated: 2022/10/07 17:51:13 by dbelarmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <fcntl.h>
+
+char	get_next_line(int fd);
+
+# endif
